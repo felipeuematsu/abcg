9671ee654559d767365244cfbe55b506042de0bf
Felipe <felipe@toke.com>
2021-10-25 19:49:01 -0300

    commit

--- examples/firstapp/main.cpp
+++ examples/firstapp/main.cpp
@@ -4,15 +4,19 @@
 #include "openglwindow.hpp"
 
 int main(int argc, char **argv) {
+  try {
     // Create application instance
     abcg::Application app(argc, argv);
-
+    
     // Create OpenGL window
     auto window{std::make_unique<OpenGLWindow>()};
     window->setWindowSettings({.title = "First App"});
-
+    
     // Run application
     app.run(std::move(window));
-
-    return 0;
+  } catch (const abcg::Exception &exception) {
+    fmt::print(stderr, "{}\n", exception.what());
+    return -1;
+  }
+  return 0;
 }
